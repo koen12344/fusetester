07cf9951a8f332fb910bf62f866505d9ef9850bd
Koen Reus <koen@koenreus.com>
2024-10-21 17:21:42 +0200

    redesign

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('fuse-tester-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/icons/icon-192x192.png',
+                '/icons/icon-512x512.png',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
